528eb00159b262df06f8a5bbec754f084377fc22
gregoire le bras <gregoire.le.bras@gmail.com>
2021-10-11 13:01:58 +0200

    fixing loading pictures

--- gender_reco/main.py
+++ gender_reco/main.py
@@ -4,22 +4,28 @@ from lxml import html
 from PIL import Image
 from mtcnn import MTCNN
 from csv import DictWriter
+from model import create_model
 import matplotlib.pyplot as plt
+import numpy as np
 import requests
 import os
 
 site="https://www.project-a.com/"
-savelocation="/data"
+savelocation=os.getcwd()+"/data"
 direc=site[12:-5]
+file_name=os.getcwd()+"/raw_data/summary_gender.csv"
+summary={}
+genmodel=create_model()
+genmodel.load_weights(os.getcwd()+"/raw_data/my_model.h5")
 
 def process_links(links):
-  """ Function to process the list of links and filter required links."""
-  links_list = []
-  formats= ["jpg", "png", "gif", "jpeg"] 
-  for link in links:
-      if link[-3:].lower() in formats:
-          links_list.append(link)
-  return links_list  
+    """ Function to process the list of links and filter required links."""
+    links_list = []
+    formats= ["jpg", "png", "gif", "jpeg"] 
+    for link in links:
+        if link[-3:].lower() in formats:
+            links_list.append(link)
+    return links_list  
 
 def listing_pages(site):
     """ Function to find all relevant pages of a website"""
@@ -63,15 +69,16 @@ def save_image(image_link):
             img_content = img_request.content
             base=f"{savelocation}/{direc}/"
             if os.path.exists(base) is False:
-                    os.mkdir(base)
-            with open(base+image_link.split('/')[-1], 'wb') as f:
+                    os.makedirs(base)
+            # with open(base+image_link.split('/')[-1], 'wb') as f:
+            with open(base+urlparse(image_link).path.split('/')[-1], 'wb') as f:                
                 byte_image = bytes(img_content)
                 f.write(byte_image)
     return base
 
 def draw_faces_quick(filename, result_list, pic):
     """ Function to save a crop of a face from a picture"""
-    crop_='/data/cropped/'
+    crop_=os.getcwd()+'/data/cropped/'
     data = plt.imread(filename)
     image_no = 1
     for i in range(len(result_list)):
@@ -88,15 +95,41 @@ def draw_faces_quick(filename, result_list, pic):
         crop_path=crop_+direc+"/"+pic.strip(f".{format}")+"/"
         name=crop_path+"/face_"+str(image_no)
         if os.path.exists(crop_) is False:
-            os.mkdir(crop_)
+            os.makedirs(crop_)
         if os.path.exists(crop_+direc+"/") is False:
-            os.mkdir(crop_+direc+"/")
+            os.makedirs(crop_+direc+"/")
         if os.path.exists(crop_path) is False:
-            os.mkdir(crop_path)
+            os.makedirs(crop_path)
         fig1.savefig(f"{name}.jpg")
         image_no+=1
 
-summary={}
+def process_and_predict(file):
+    im = Image.open(file)
+    width, height = im.size
+    if width == height:
+        im = im.resize((200,200), Image.ANTIALIAS)
+    else:
+        if width > height:
+            left = width/2 - height/2
+            right = width/2 + height/2
+            top = 0
+            bottom = height
+            im = im.crop((left,top,right,bottom))
+            im = im.resize((200,200), Image.ANTIALIAS)
+        else:
+            left = 0
+            right = width
+            top = 0
+            bottom = width
+            im = im.crop((left,top,right,bottom))
+            im = im.resize((200,200), Image.ANTIALIAS)
+            
+    ar = np.asarray(im)
+    ar = ar.astype('float32')
+    ar /= 255.0
+    ar = ar.reshape(-1, 200, 200, 3)
+    return ar
+
 pictures_treated=0
 directory_processed=0
 faces_num=[]
@@ -121,6 +154,7 @@ for x in imlist:
         rgb_im = im1.convert('RGB')
         rgb_im.save(x+".jpg")
         x=x+".jpg"
+    pictures_treated+=1
     pic=x.replace(base,"")
     filename=base+pic
     pixels = plt.imread(filename)
@@ -137,73 +171,35 @@ for x in imlist:
         pictures_treated_wo_faces+=1
     draw_faces_quick(filename, faces, pic)
 
-def process_and_predict_amu(file):
-    """ Function to predict the gender of a face"""
-    im = Image.open(file)
-    width, height = im.size
-    if width == height:
-        im = im.resize((200,200), Image.ANTIALIAS)
-    else:
-        if width > height:
-            left = width/2 - height/2
-            right = width/2 + height/2
-            top = 0
-            bottom = height
-            im = im.crop((left,top,right,bottom))
-            im = im.resize((200,200), Image.ANTIALIAS)
-        else:
-            left = 0
-            right = width
-            top = 0
-            bottom = width
-            im = im.crop((left,top,right,bottom))
-            im = im.resize((200,200), Image.ANTIALIAS)
-            
-    ar = np.asarray(im)
-    ar = ar.astype('float32')
-    # ar /= 255.0
-    ar = ar.reshape(-1, 200, 200, 3)
-    
-    # age = agemodel.predict(ar)
-    # gender = np.round(genmodel.predict(ar))
-    gender = np.round(model.predict(ar))
-    if gender == 0:
-        gender = 'male'
-    elif gender == 1:
-        gender = 'female'
-        
-    print('Gender:', gender)
-    # plt.imshow(im)
-    return im.resize((300,300), Image.ANTIALIAS)
 
-
-directo_list=list(filter(os.path.isdir, [os.path.join(base, f) for f in os.listdir(base)]))    
+cropped_dir=savelocation+"/cropped/"+f"{direc}"
+directo_list=list(filter(os.path.isdir, [os.path.join(cropped_dir, f) for f in os.listdir(cropped_dir)]))    
 
 for x in directo_list:
     directory_processed+=1
-for v in os.listdir(x):    
-    if ".jpg" in v:
-        filepic=x+"/"+v
-        ar=process_and_predict(filepic)
-        gender = np.round(genmodel.predict(ar))    
-    if gender == 0:
-        male+=1
-    elif gender == 1:
-        female+=1
+    for v in os.listdir(x):    
+        if ".jpg" in v:
+            filepic=x+"/"+v
+            ar=process_and_predict(filepic)
+            gender = np.round(genmodel.predict(ar))    
+        if gender == 0:
+            male+=1
+        elif gender == 1:
+            female+=1
 
-summary["Site"]=direc
-summary["pictures_treated"]=pictures_treated
-summary["directory_processed"]=directory_processed
-summary["faces_num"]=sum(faces_num)
-summary["pictures_treated_wo_faces"]=pictures_treated_wo_faces
-summary["male"]=male
-summary["female"]=female
-summary["picture_w_faces"]=picture_w_faces
-if picture_w_faces !=0:
-    summary["woman_gender_ratio"]=(female/picture_w_faces)/(male/picture_w_faces)
-else: 
-    summary["woman_gender_ratio"]="NaN"
-summary={}
+    summary["Site"]=direc
+    summary["pictures_treated"]=pictures_treated
+    summary["directory_processed"]=directory_processed
+    summary["faces_num"]=sum(faces_num)
+    summary["pictures_treated_wo_faces"]=pictures_treated_wo_faces
+    summary["male"]=male
+    summary["female"]=female
+    summary["picture_w_faces"]=picture_w_faces
+    if picture_w_faces !=0:
+        summary["woman_gender_ratio"]=(female/picture_w_faces)/(male/picture_w_faces)
+    else: 
+        summary["woman_gender_ratio"]="NaN"
+
     if os.path.isfile(file_name):
         with open(file_name, 'a', newline='') as write_obj:
             dict_writer = DictWriter(write_obj, fieldnames=summary.keys())
@@ -215,5 +211,3 @@ summary={}
             dict_writer.writerow(summary) 
 
 
-
-  pictures_treated+=1
